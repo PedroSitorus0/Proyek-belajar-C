e4147e7db10761bab36ecafa0dc998d8b21dab08
Pedro <pedrositorus0@gmail.com>
2026-02-13 12:54:15 +0700

    Refactor math.c to use float for calculations and update printf format

--- math.c
+++ math.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h> // jangan lupaa ditambah jikaa ingin melakukan perhitungan abs
 
 int main () {
 
-    int x = 2;
-    
+    // float x = 3.14;
+    float x = 45;
     // x = sqrt(x); //square root(akar)
     // x = pow(x, 3); //power(pangkat)
-
-    printf("%d\n", x);
+    // x = round(x); //round(pembulatan ke bilangan terdektat)
+    // x = ceil(x); //ceiling(pembulatan ke bilangan tertinggi)
+    // x = floor(x);    //floor (pembulatan ke bilangan terendah)
+    // x = abs(x); //pembalik dari negatif ke positif (hanya jika negatif)
+    // x = log(x); //logaritma
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
+    printf("%f\n", x); //ganti %lf dengan %d/i jika ingin menjumlahkan bilangan yang sudah beberntuk desimal
     printf("hello world\n");
     
 
