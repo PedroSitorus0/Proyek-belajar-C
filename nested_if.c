bc44b52a00ed6af1d75c48144d0b7d3b2f1dcf76
Pedro <pedrositorus0@gmail.com>
2026-02-15 21:55:17 +0700

    Fix student and senior discount logic in nested if structure

--- nested_if.c
+++ nested_if.c
@@ -3,15 +3,30 @@
 
 int main() {
     float price = 10.00;
-    bool isStudent = true;
+    bool isStudent=  false ;
+    bool isSenior= true;
     int yep = 0;
     char currency[10] = "Rp";
     if (isStudent)
     {
-        printf("you get a student discount of 10%");
-        price *= price;
-    }   
+        if (isSenior)
+        {
+            printf("You get a senior and a student discount of 20%\n");
+            price *= 0.7;
+        }
+        else {
+        printf("you get a student discount of 10%\n");
+        price *= 0.9;
+        }
+    } else {
+        if (isSenior)
+        {
+            printf("You get a senior discount of 20%\n");
+            price *= 0.8;
+        }
+        
+    }
 
-    printf("The price of a ticket is: %s%.2lf", currency ,price);
+    printf("The price of a ticket is: %s%.2lf\n", currency ,price);
     return yep;
 }
