f7a64c2bf9f59185c2ec81c30d43ece15493bf5d
Pedro <pedrositorus0@gmail.com>
2026-02-16 19:12:29 +0700

    fixed

---
+++ function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void hello(char name[50], int age) {
+
+    printf("happy birthday %s", name);
+    printf("your age is %d\n", age);
+    printf("I hate you!!!\n");
+}
+
+int main (int *argv[]) {
+
+    int yep = 0;
+    int invalid = 1;
+    int age;
+    char name[50] = "";
+
+    printf("masukan nama anda: "); fgets(name, sizeof(name),stdin);
+    printf("masukan usia anda: "); scanf("%d", &age);
+
+    hello(name, age);
+    hello(name, age);
+    hello(name, age);
+    hello(name, age);
+
+
+
+    return yep;
+}
