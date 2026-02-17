6e7abb83fcf0e2e2c2b7a70fe19f10a7278a126e
Pedro <pedrositorus0@gmail.com>
2026-02-17 20:31:30 +0700

    fixed

--- variableScope.c
+++ variableScope.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
+void yeah(char name[], int age); //funciton prototype
+bool ageCheck(int age);
 
 int main() {
     int yep = 0;
+    yeah("ikan", 13);
     
-
-
+    if (ageCheck(32))
+    {
+        printf("anda dapat berkerja di perusahaan ini\n");
+    } else {
+        printf("maaf tapi usia anda belum memenuhi syarat yang ada\n");
+    }
+    
+    
     return yep;
+}
+
+void yeah(char name[], int age) {
+    printf("hello %s\n",name);
+    printf("you are %d years old.\n", age);
+}
+
+bool ageCheck(int age) {
+   return age >= 16;
 }
